c18b7481b69dc69a06b46fa573aa6de3edeb5564
carlosevi94 <carlosevillabarcelo@gmail.com>
2019-01-28 19:32:30 +0100

    test git

--- transform_categorical.py
+++ transform_categorical.py
@@ -24,6 +24,7 @@ data = data.fillna( { 'Census_PrimaryDiskTypeName':'UNKNOWN'} )
 indexer = StringIndexer(inputCol="Census_PrimaryDiskTypeName", outputCol="Census_PrimaryDiskTypeNameIndex")
 data = indexer.fit(data).transform(data)
 
+test
 
 #######
 
