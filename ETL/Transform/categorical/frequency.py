e497a0932ee3701a267cf04ea3fde544d264f13e
Carlos Sevilla Barceló <carsevbar@gmail.com>
2019-02-07 11:02:31 +0100

    Frecuency: Codigo organizado

--- ETL/Transform/categorical/frequency.py
+++ ETL/Transform/categorical/frequency.py
@@ -47,9 +47,12 @@ init_cols = data.columns
 # Transformaciones
 print('Inicio de las transformaciones:')
 
+
+# =============================================================================
+# Census_ChassisTypeName
+# 	Frecuencia
+# =============================================================================
 print('\tCensus_ChassisTypeName')
-## Census_ChassisTypeName
-	# Frecuencia
 frequency_census = data.groupBy('Census_ChassisTypeName').count().withColumnRenamed('count','Census_ChassisTypeName_freq')
 data = data.join(frequency_census,'Census_ChassisTypeName','left').drop('Census_ChassisTypeName')
 
@@ -57,82 +60,109 @@ data.persist()
 print(data.first())
 
 
+# =============================================================================
+# Census_InternalBatteryType
+# 	Frecuencia
+# =============================================================================
 print('\tCensus_InternalBatteryType bool')
-## Census_InternalBatteryType
-	# Frecuencia
 frequency_census = data.groupBy('Census_InternalBatteryType').count().withColumnRenamed('count','Census_InternalBatteryType_freq')
 data = data.join(frequency_census,'Census_InternalBatteryType','left')
 
-	#Booleana. QUITAR EN EL FUTURO EL NULL
+# 	Booleana. QUITAR EN EL FUTURO EL NULL
 data = data.withColumn('Census_InternalBatteryType_informed', when(col('Census_InternalBatteryType').isNotNull(),1).otherwise(0)).drop('Census_InternalBatteryType')
 
 data.persist()
 print(data.first())
 
+
+# =============================================================================
+# Census_OSBranch
+# 	frequency
+# =============================================================================
 print('\tCensus_OSBranch')
-## Census_OSBranch
-	# frequency
 frequency_census = data.groupBy('Census_OSBranch').count().withColumnRenamed('count','Census_OSBranch_freq')
 data = data.join(frequency_census,'Census_OSBranch','left').drop('Census_OSBranch')
 
 data.persist()
 print(data.first())
 
+
+# =============================================================================
+# Census_OSEdition
+# 	Frecuencia
+# =============================================================================
 print('\tCensus_OSEdition')
-## Census_OSEdition
-	# Frecuencia
 df_cat_freq_Census_OSEdition = data.groupBy('Census_OSEdition').count().withColumnRenamed('count', 'Census_OSEdition_freq')
 data = data.join(df_cat_freq_Census_OSEdition, ['Census_OSEdition'], 'left').drop('Census_OSEdition')
 
 data.persist()
 print(data.first())
 
+
+# =============================================================================
+# Census_OSSkuName
+# 	Frecuencia
+# =============================================================================
 print('\tCensus_OSSkuName')
-## Census_OSSkuName
-	# Frecuencia
 frequency_Census_OSSkuName = data.groupBy('Census_OSSkuName').count().withColumnRenamed('count','Census_OSSkuName_freq')
 data = data.join(frequency_Census_OSSkuName,'Census_OSSkuName','left').drop('Census_OSSkuName')
 
 data.persist()
 print(data.first())
 
+
+# =============================================================================
+# Census_FlightRing
+# 	Frecuencia
+# =============================================================================
 print('\tCensus_FlightRing')
-## Census_FlightRing
-	# Frecuencia
 frequency_Census_Census_FlightRing = data.groupBy('Census_FlightRing').count().withColumnRenamed('count','Census_FlightRing_freq')
 data = data.join(frequency_Census_Census_FlightRing,'Census_FlightRing','left').drop('Census_FlightRing')
 
 data.persist()
 print(data.first())
 
+
+# =============================================================================
+# OsVer
+# 	Frecuencia
+# =============================================================================
 print('\tOsVer')
-## OsVer
-	# Frecuencia
 df_cat_freq_osver = data.groupBy('OsVer').count().withColumnRenamed('count', 'OsVer_freq')
 data = data.join(df_cat_freq_osver, ['OsVer'], 'left').drop('OsVer')
 
 data.persist()
 print(data.first())
 
+
+# =============================================================================
+# SmartScreen
+# 	Frecuencia
+# =============================================================================
 print('\tSmartScreen')
-## SmartScreen
-	# Frecuencia
 df_cat_freq_SmartScreen = data.groupBy('SmartScreen').count().withColumnRenamed('count', 'SmartScreen_freq')
 data = data.join(df_cat_freq_SmartScreen, ['SmartScreen'], 'left').drop('SmartScreen')
 
 data.persist()
 print(data.first())
 
+
+# =============================================================================
+# Census_MDC2FormFactor
+# 	Frecuencia
+# =============================================================================
 print('\tCensus_MDC2FormFactor')
-## Census_MDC2FormFactor
-	# Frecuencia
 df_cat_freq_osver = data.groupBy('Census_MDC2FormFactor').count().withColumnRenamed('count', 'Census_MDC2FormFactor_freq')
 data = data.join(df_cat_freq_osver, ['Census_MDC2FormFactor'], 'left').drop('Census_MDC2FormFactor')
 
 data.persist()
 print(data.first())
 
+
+# =============================================================================
+# Guardamos el DataFrame
 # Guardamos el DF con las variables categoricas transformadas
+# =============================================================================
 final_cols = data.columns
 cols_transformadas = list(set(final_cols) - set(init_cols))
 
