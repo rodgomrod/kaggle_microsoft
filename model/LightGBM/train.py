719ba07a5c6e0a78dcf79699900608d5e1c8df8c
rodgomrod <rodgomrod@gmail.com>
2019-02-12 20:06:16 +0100

    Random Forest OK, LightGBM OK, XGBoost NO

--- model/LightGBM/train.py
+++ model/LightGBM/train.py
@@ -1,18 +1,15 @@
-import os
-os.environ['PYSPARK_SUBMIT_ARGS'] = '--jars ../../jars/xgboost4j-spark-0.72.jar,../../jars/xgboost4j-0.72.jar pyspark-shell'
+# PARA HACERLO ANDAR: pyspark --packages Azure:mmlspark:0.15   |    en consola
+
 from pyspark.sql import SparkSession
 from pyspark.ml import Pipeline
 from pyspark.ml.feature import VectorAssembler
 from pyspark.ml.evaluation import BinaryClassificationEvaluator
 from pyspark.ml.tuning import CrossValidator, ParamGridBuilder
 from pyspark.ml.classification import RandomForestClassifier
+from mmlspark import LightGBMClassifier
+from mmlspark.ComputeModelStatistics import ComputeModelStatistics
 
-spark = SparkSession.builder.appName('XGB_trainer').getOrCreate()
-
-spark.sparkContext.addPyFile("/home/errodringer/Descargas/sparkxgb.zip")
-from sparkxgb import XGBoostEstimator
-
-spark = SparkSession.builder.appName('RF_trainer').getOrCreate()
+spark = SparkSession.builder.appName('LGBM_trainer').getOrCreate()
 
 path_train = '../../data/train_final_0'
 print('Carga del TRAIN', path_train)
@@ -29,24 +26,33 @@ train_cols = [c for c in train.columns if c not in ignore_c]
 # Convertimos el TRAIN en un VECTOR para poder pasarle el RF
 print('Conversion de datos a VectorAssembler')
 assembler_features = VectorAssembler(inputCols=train_cols, outputCol='features')
+
 train_2 = train.limit(10000)
 train_data = assembler_features.transform(train_2)
 train_data = train_data.select('features', 'HasDetections')\
     .withColumnRenamed('HasDetections', 'label')
 
-
-xgboost = XGBoostEstimator(
-    featuresCol="features",
-    labelCol="label",
-    predictionCol="prediction"
-)
-
-pipeline = Pipeline().setStages([xgboost])
-
 trainDF, testDF = train_data.randomSplit([0.8, 0.2], seed=24)
 
-model = pipeline.fit(trainDF)
-preds = model.transform(testDF)
+model = LightGBMClassifier(learningRate=0.1,
+                           numIterations=30,
+                           numLeaves=20,
+                           objective="binary",
+                           maxDepth=10
+                           )
 
-preds.select(col("label"), col("prediction")).show()
-preds.show()
+model_trained = model.fit(trainDF)
+
+prediction = model_trained.transform(testDF)
+
+metrics = ComputeModelStatistics().transform(prediction)
+metrics.select('accuracy').show()
+
+# >>> d = [{'name': 'Alice', 'age': 1}]
+# >>> spark.createDataFrame(d).collect()
+
+
+# FEATURES IMPORTANCES
+# for i, j in zip(model.getFeatureImportances(), train_cols):
+#     if i > 0:
+#         print(i,j)
