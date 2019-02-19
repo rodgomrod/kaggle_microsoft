ef0b9c7e8c0113837c91f47e88b29fd9ba2093f4
Carlos Sevilla Barceló <carlosevillabarcelo@gmail.com>
2019-02-19 17:44:06 +0100

    AWS Enviroment & Launcher

---
+++ aws_env.sh
@@ -0,0 +1,22 @@
+
+
+git clone https://github.com/rodgomrod/kaggle_microsoft
+
+cd kaggle_microsoft
+
+pip3 install -r requirements2.txt
+
+mkdir data
+
+
+cd ..
+
+mv train.csv /kaggle_microsoft/data/train.csv
+
+mv test.csv /kaggle_microsoft/data/test.csv
+
+cd kaggle_microsoft
+
+chmod +x tablon2.sh
+
+./tablon2.sh
--- tablon_amazon.sh
+++ tablon_amazon.sh
@@ -4,19 +4,34 @@ echo "Generando DFs con vars categoricas y numericas"
 python3 ETL/Extract/numerical_categoric_dataset.py
 echo "-------------------"
 
+
+
+
 echo -e "\e[92m\e[5mPreprocesamos variables categoricas\e[0m"
 python3 ETL/Preprocessing/categorical.py
 echo "-------------------"
 
+rm -rf data/df_cat/
+
+
+
 echo -e "\e[92m\e[5mProcesamos variables categoricas\e[0m"
 python3 ETL/Transform/categorical/global.py
 echo "-------------------"
+
+rm -rf data/df_cat_prepro_0/
 
 
 echo -e "\e[92m\e[5mImputamos valores numericos\e[0m"
 python3 ETL/Transform/numeric/impute_numerical.py
 echo "-------------------"
 
+rm -rf data/df_num/
+
+
 echo -e "\e[92m\e[5mGeneramos TRAIN / TEST de las variables nuevas\e[0m"
 python3 ETL/Load/train_test_new_variables.py
 echo "-------------------"
+
+rm -rf  data/df_cat_pro_0/
+rm -rf  data/df_num_imputed_0/
