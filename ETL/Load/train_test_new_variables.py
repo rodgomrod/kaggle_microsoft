85ac6e0c9327c3de21a0836a334c82fe7fa4096c
rodgomrod <rodgomrod@gmail.com>
2019-02-20 21:36:20 +0100

    Inicio automatizacion del entrenamiento de modelos

--- ETL/Load/train_test_new_variables.py
+++ ETL/Load/train_test_new_variables.py
@@ -11,8 +11,8 @@ import multiprocessing
 cores = multiprocessing.cpu_count()
 p = 2
 conf = SparkConf()
-conf.set("spark.driver.cores", cores)
-conf.set("spark.driver.memory", "55g")
+# conf.set("spark.driver.cores", cores)
+conf.set("spark.driver.memory", "12g")
 conf.set("spark.sql.shuffle.partitions", p * cores)
 conf.set("spark.default.parallelism", p * cores)
 sc = SparkContext(conf=conf)
@@ -21,14 +21,27 @@ sc = SparkContext(conf=conf)
 spark = SparkSession.builder.appName('MK_genera_train_test').getOrCreate()
 
 df_num = spark.read.csv('data/df_num_imputed_0/*.csv', header=True, inferSchema=True)
-df_num.persist()
-df_num.count()
+# df_num.persist()
+# df_num.count()
 
-df_cat = spark.read.csv('data/df_cat_pro_0/*.csv', header=True, inferSchema=True)
-df_cat.persist()
-df_cat.count()
+cat_cols = ['MachineIdentifier', 'Census_InternalBatteryType_informed', 'ProductName_index', 'Platform_index',
+            'Processor_index', 'OsPlatformSubRelease_index', 'SkuEdition_index', 'PuaMode_index', 'SmartScreen_index',
+            'Census_MDC2FormFactor_index', 'Census_DeviceFamily_index', 'Census_ProcessorClass_index',
+            'Census_PrimaryDiskTypeName_index', 'Census_ChassisTypeName_index', 'Census_PowerPlatformRoleName_index',
+            'Census_InternalBatteryType_index', 'Census_OSArchitecture_index', 'Census_OSBranch_index',
+            'Census_OSEdition_index', 'Census_OSSkuName_index', 'Census_OSInstallTypeName_index',
+            'Census_OSWUAutoUpdateOptionsName_index', 'Census_GenuineStateName_index', 'Census_ActivationChannel_index',
+            'Census_FlightRing_index']
 
-full_df = df_num.join(df_cat, ['MachineIdentifier'])
+df_cat = spark.read.csv('data/df_cat_pro_0/*.csv', header=True, inferSchema=True).select(cat_cols)
+# df_cat.persist()
+# df_cat.count()
+
+df_dates = spark.read.csv('data/df_dates_0/*.csv', header=True, inferSchema=True)
+
+full_df_1 = df_num.join(df_cat, ['MachineIdentifier'])
+
+full_df = full_df_1.join(df_dates, ['MachineIdentifier'])
 
 train = full_df.filter(col('HasDetections').isNotNull())
 train = train.fillna(-99)
@@ -37,5 +50,5 @@ test = test.fillna(-99)
 
 write_path_train = 'data/train_final_1'
 write_path_test = 'data/test_final_1'
-train.coalesce(1).write.csv(write_path_train, sep=',', mode="overwrite", header=True)
-test.coalesce(1).write.csv(write_path_test, sep=',', mode="overwrite", header=True)
+train.write.csv(write_path_train, sep=',', mode="overwrite", header=True)
+test.write.csv(write_path_test, sep=',', mode="overwrite", header=True)
--- ETL/Transform/dates/dates.py
+++ ETL/Transform/dates/dates.py
@@ -11,29 +11,33 @@ import multiprocessing
 conf = SparkConf()
 cores = multiprocessing.cpu_count()
 conf = SparkConf()
-conf.set("spark.sql.shuffle.partitions", int(240))
-conf.set("spark.default.parallelism", int(240))
-conf.set("spark.driver.cores", int(8))
-conf.set("sspark.executor.cores ", int(8))
-conf.set("spark.driver.memory", '12g')
-conf.set("spark.executor.memory", '12g')
+conf.set("spark.sql.shuffle.partitions", int(800))
+conf.set("spark.default.parallelism", int(800))
+# conf.set("spark.driver.cores", int(8))
+# conf.set("spark.executor.cores ", int(8))
+conf.set("spark.driver.memory", '15g')
+# conf.set("spark.executor.memory", '15g')
 
 sc = SparkContext(conf=conf)
 
 spark = SparkSession.builder.appName("Microsoft_Kaggle").getOrCreate()
 
-df_num = spark.read.csv("../../../data/df_cat_prepro_0/*.csv",inferSchema=True,header=True)
+df_num = spark.read.csv("../../../data/df_cat_prepro_0/*.csv",inferSchema=True,header=True)\
+    .select('MachineIdentifier','Platform', 'OsBuildLab', 'AvSigVersion')
 df_num.persist()
 df_num.count()
+print('DF leido')
 
 df_fechas_av = spark.read.csv("../../../Notebooks/fechas_av.csv",inferSchema=True,header=True)
-df_fechas_os = spark.read.csv("../../../Notebooks/fechas_os.csv",inferSchema=True,header=True)
+# df_fechas_os = spark.read.csv("../../../Notebooks/fechas_os.csv",inferSchema=True,header=True)
 
-df_fechas_os = df_fechas_os.withColumn('DateCensus_OSVersion', to_date(col('DateCensus_OSVersion')))
+# df_fechas_os = df_fechas_os.withColumn('DateCensus_OSVersion', to_date(col('DateCensus_OSVersion')))
 df_fechas_av = df_fechas_av.withColumn('DateAvSigVersion', to_date(col('DateAvSigVersion')))
 
-df_fechas_os = df_fechas_os.withColumn('DateCensus_OSVersion', to_date(col('DateCensus_OSVersion')))
+# df_fechas_os = df_fechas_os.withColumn('DateCensus_OSVersion', to_date(col('DateCensus_OSVersion')))
 df_fechas_av = df_fechas_av.withColumn('DateAvSigVersion', to_date(col('DateAvSigVersion')))
+
+print('Fechas AV leido')
 
 df_date_osbuild = df_num.withColumn('OsBuildLab_4', split(df_num['OsBuildLab'], '\.')[4].cast(StringType()))
 df_date_osbuild = df_date_osbuild.withColumn('OsBuildLab_date', split(df_date_osbuild['OsBuildLab_4'], '-')[0].cast(StringType()))
@@ -45,34 +49,41 @@ df_dates = df_date_osbuild.join(df_fechas_av, ['AvSigVersion'], 'left').select('
                                                                                'OsBuildLab_date',
                                                                                'DateAvSigVersion')
 
+print("join entre AV dates y DF")
+
 w1 = Window.partitionBy('Platform').orderBy('OsBuildLab_date')
 w2 = Window.partitionBy('Platform').orderBy('DateAvSigVersion')
 w3 = Window.partitionBy().orderBy('OsBuildLab_date')
 w4 = Window.partitionBy().orderBy('DateAvSigVersion')
 
 data_windows = df_dates.withColumn('OsBuildLab_date_lag', lag('OsBuildLab_date').over(w1))
-data_windows.persist()
-data_windows.count()
+# data_windows.persist()
+# data_windows.count()
+print("window 1")
 
-df_num.unpersist()
+# df_num.unpersist()
+# spark.catalog.clearCache()
 
 date_diff = data_windows.withColumn('OSBuild_diff', datediff(col('OsBuildLab_date'), col('OsBuildLab_date_lag')))
 
 date_diff_2 = date_diff.withColumn('DateAvSigVersion_lag', lag('DateAvSigVersion').over(w2))
-date_diff_2.persist()
-date_diff_2.count()
+# date_diff_2.persist()
+# date_diff_2.count()
+print("window 2")
 
 date_diff_3 = date_diff_2.withColumn('AvSigVersion_diff', datediff(col('DateAvSigVersion'), col('DateAvSigVersion_lag')))
 
 data_windows = date_diff_3.withColumn('OsBuildLab_date_fulllag', lag('OsBuildLab_date').over(w3))
-data_windows.persist()
-data_windows.count()
+# data_windows.persist()
+# data_windows.count()
+print("window 3")
 
 data_windows = data_windows.withColumn('OSBuild_fulldiff', datediff(col('OsBuildLab_date'), col('OsBuildLab_date_fulllag')))
 
 data_windows = data_windows.withColumn('DateAvSigVersion_fulllag', lag('DateAvSigVersion').over(w4))
 data_windows.persist()
 data_windows.count()
+print("window 4")
 
 data_windows = data_windows.withColumn('AvSigVersion_fulldiff', datediff(col('DateAvSigVersion'), col('DateAvSigVersion_fulllag')))
 
@@ -86,6 +97,8 @@ df_max_date = data_windows.groupBy('Platform').agg(max('OsBuildLab_date'),
 
 df_date_max_date = data_windows.join(df_max_date, ['Platform'], 'left')
 
+print("ultimo join")
+
 df_date_max_date = df_date_max_date.withColumn('OsBuildLab_difftotal', datediff(col('max(OsBuildLab_date)'), col('OsBuildLab_date')))\
 .withColumn('DateAvSigVersion_difftotal', datediff(col('max(DateAvSigVersion)'), col('DateAvSigVersion')))\
 .withColumn('DateAvSigVersion_fulldifftotal', datediff(col('max(DateAvSigVersion)'), col('DateAvSigVersion_fulllag')))\
@@ -97,6 +110,9 @@ df_date_max_date = df_date_max_date.withColumn('OsBuildLab_difftotal', datediff(
 
 final_dates = df_date_max_date.withColumn('OsBuildLab_dayOfWeek', date_format('OsBuildLab_date', 'u'))\
 .withColumn('AvSigVersion_dayOfWeek', date_format('DateAvSigVersion', 'u'))
+
+final_dates.persist()
+final_dates.count()
 
 drop_list = ['Platform', 'OsBuildLab_date', 'DateAvSigVersion', 'OsBuildLab_date_lag',
              'DateAvSigVersion_lag', 'max(OsBuildLab_date)', 'max(DateAvSigVersion)',
