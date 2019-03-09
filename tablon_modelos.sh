afcb5b34282a1cdf96b2187d9078ee133980f209
Rodrigo <rodgomrod@gmail.com>
2019-03-09 17:03:16 +0100

    actualizacion tablon modelos (arreglado)

--- tablon_modelos.sh
+++ tablon_modelos.sh
@@ -1,18 +1,7 @@
 #!/usr/bin/env bash
 
 
-params="{'max_depth': 10,
-'n_estimators': 10000,
-'learning_rate': 0.07,
-'num_leaves': 31,
-'colsample_bytree': 0.2,
-'objective': 'binary',
-'n_jobs': -1,
-'seed': 42,
-'bagging_fraction': 0.5,
-'bagging_freq': 10,
-'lambda_l1': 0.2,
-'lambda_l2': 0.2}"
+params="{'max_depth':10,'n_estimators':10000,'learning_rate':0.07,'num_leaves':31,'colsample_bytree':0.2,'objective':'binary','n_jobs':-1,'seed':42,'bagging_fraction':0.5,'bagging_freq':10,'lambda_l1':0.2,'lambda_l2':0.2}"
 
 k="3"
 
@@ -36,14 +25,7 @@ echo ""
 echo ""
 echo ""
 
-params="{'max_depth': 13,
-'n_estimators': 10000,
-'learning_rate': 0.09,
-'num_leaves': 31,
-'colsample_bytree': 0.2,
-'objective': 'binary',
-'n_jobs': -1,
-'seed': 42}"
+params="{'max_depth':13,'n_estimators':10000,'learning_rate':0.09,'num_leaves':31,'colsample_bytree':0.2,'objective':'binary','n_jobs':-1,'seed':42}"
 
 k="3"
 
@@ -61,24 +43,12 @@ python3 model/LightGBM/sklearn/test.py ${k} ${drop_version} ${model_name}
 echo ""
 
 echo -e "\e[92m\e[5mSubmitting predictions \e[34m${model_name}\e[0m"
-kaggle competitions submit -c microsoft-malware-prediction -f submissions/${model_name}.csv -m "Nuevos datos V4:
- sin versiones - ${params}"
+kaggle competitions submit -c microsoft-malware-prediction -f submissions/${model_name}.csv -m "Nuevos datos V4: sin versiones - ${params}"
 echo ""
 echo ""
 echo ""
 
-params="{'max_depth': 10,
-'n_estimators': 10000,
-'learning_rate': 0.09,
-'num_leaves': 31,
-'colsample_bytree': 0.2,
-'objective': 'binary',
-'n_jobs': -1,
-'seed': 42,
-'bagging_fraction': 0.5,
-'bagging_freq': 10,
-'lambda_l1': 0.1,
-'lambda_l2': 0.1}"
+params="{'max_depth':10,'n_estimators':10000,'learning_rate':0.09,'num_leaves':31,'colsample_bytree':0.2,'objective':'binary','n_jobs':-1,'seed':42,'bagging_fraction':0.5,'bagging_freq':10,'lambda_l1':0.1,'lambda_l2':0.1}"
 
 k="3"
 
@@ -96,8 +66,7 @@ python3 model/LightGBM/sklearn/test.py ${k} ${drop_version} ${model_name}
 echo ""
 
 echo -e "\e[92m\e[5mSubmitting predictions \e[34m${model_name}\e[0m"
-kaggle competitions submit -c microsoft-malware-prediction -f submissions/${model_name}.csv -m "Nuevos datos V4:
- sin versiones - ${params}"
+kaggle competitions submit -c microsoft-malware-prediction -f submissions/${model_name}.csv -m "Nuevos datos V4: con versiones - ${params}"
 echo ""
 echo ""
 echo ""
